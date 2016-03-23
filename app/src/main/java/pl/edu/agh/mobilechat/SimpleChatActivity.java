e43b0514f64f0cdb5cbc6281003e4e322a5f62e0
liscju <piotr.listkiewicz@gmail.com>
2016-03-23 12:36:07 +0100

    Created gui skeleton, now sends messages locally

--- app/src/main/java/pl/edu/agh/mobilechat/SimpleChatActivity.java
+++ app/src/main/java/pl/edu/agh/mobilechat/SimpleChatActivity.java
@@ -1,16 +1,32 @@
 package pl.edu.agh.mobilechat;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SimpleChatActivity extends AppCompatActivity {
     private String nick;
     private String ip;
+
+    private ArrayList<String> listItems = new ArrayList<String>();
+    private ArrayAdapter<String> adapter;
+
+    private TextView nickTextView;
+    private ListView chatListView;
+    private EditText messageEditText;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +39,37 @@ public class SimpleChatActivity extends AppCompatActivity {
         ip = getIntent().getStringExtra(ConnectActivity.IP);
 
 
-        TextView nickTextView = (TextView) findViewById(R.id.nickTextView);
+        nickTextView = (TextView) findViewById(R.id.nickTextView);
+        chatListView = (ListView) findViewById(R.id.chatListView);
+        messageEditText = (EditText) findViewById(R.id.messageEditText);
+        sendButton = (Button) findViewById(R.id.sendButton);
+
         nickTextView.setText(nick + "@" + ip);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
+        chatListView.setAdapter(adapter);
+
+        final Handler myHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                listItems.add("[" + msg.getData().getString("NICK") + "]" +
+                    msg.getData().getString("MSG"));
+                adapter.notifyDataSetChanged();
+                chatListView.setSelection(listItems.size() - 1);
+            }
+        };
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Message msg = myHandler.obtainMessage();
+                Bundle b = new Bundle();
+                b.putString("NICK", "JA");
+                b.putString("MSG", messageEditText.getText().toString());
+                msg.setData(b);
+                myHandler.sendMessage(msg);
+            }
+        });
     }
 
 }
