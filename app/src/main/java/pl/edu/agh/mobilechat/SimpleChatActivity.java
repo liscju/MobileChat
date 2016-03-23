2a38d4130b3c66607b5090475286257e60cb28ca
liscju <piotr.listkiewicz@gmail.com>
2016-03-23 12:49:33 +0100

    Add receiving messages from server

--- app/src/main/java/pl/edu/agh/mobilechat/SimpleChatActivity.java
+++ app/src/main/java/pl/edu/agh/mobilechat/SimpleChatActivity.java
@@ -14,9 +14,19 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
 import java.util.ArrayList;
 
 public class SimpleChatActivity extends AppCompatActivity {
+    MqttClient sampleClient=null;
+
     private String nick;
     private String ip;
 
@@ -27,6 +37,7 @@ public class SimpleChatActivity extends AppCompatActivity {
     private ListView chatListView;
     private EditText messageEditText;
     private Button sendButton;
+    private Handler myHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,7 +60,7 @@ public class SimpleChatActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItems);
         chatListView.setAdapter(adapter);
 
-        final Handler myHandler = new Handler() {
+        myHandler = new Handler() {
             @Override
             public void handleMessage(Message msg) {
                 listItems.add("[" + msg.getData().getString("NICK") + "]" +
@@ -70,6 +81,66 @@ public class SimpleChatActivity extends AppCompatActivity {
                 myHandler.sendMessage(msg);
             }
         });
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                startMQTT();
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (sampleClient != null) {
+            try {
+                sampleClient.disconnect();
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void startMQTT(){
+        String clientId;
+        MemoryPersistence persistence = new MemoryPersistence();
+        try {
+            String broker = "tcp://"+ip+":1883";
+            clientId = nick;
+            sampleClient = new MqttClient(broker, clientId, persistence);
+            sampleClient.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable throwable) {
+
+                }
+
+                public void messageArrived(String s, MqttMessage mqttMessage) throws
+                        Exception {
+                    Message msg = myHandler.obtainMessage();
+                    Bundle b = new Bundle();
+                    b.putString("NICK", nick);
+                    b.putString("MSG", mqttMessage.toString());
+                    msg.setData(b);
+                    myHandler.sendMessage(msg);
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+                }
+                //TODO
+            });
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            sampleClient.subscribe("#");
+        } catch (MqttException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
 }
